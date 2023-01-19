080e0c1f180220a42229b83c89bae894f6973c46
Atryom <artem@hopshop.ru>
2023-01-19 14:11:06 +0300

    add loader and error message

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,87 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import { Data, getCommits } from '../src/fetch/commits'
+import { formatDate } from '../src/utils/formatDate'
 import { UpdateIcon } from '../public/update.icon'
+import React, { useState } from 'react'
+import { Loader } from '../src/components/Loader'
 
-const Home: NextPage<{ commits: Data[] }> = (props) => {
+const Home: NextPage<{ commits: Data[] }> = ({
+  commits
+}) => {
 
-  const formatDate = (date: string) => {
-    const currentDate = new Date(date)
-    const options: Intl.DateTimeFormatOptions = { month: 'short', day: 'numeric', hour: "2-digit", minute: '2-digit' };
-    console.log(typeof currentDate.toLocaleDateString("en-US", options))
-    return currentDate.toLocaleDateString("en-US", options)
+  const [currentCommits, setCurrentCommits] = useState(commits)
+  const [error, setError] = useState<unknown>()
+  const [loading, setLoading] = useState(false)
+
+  const loadNewCommitsList = async () => {
+    try {
+      setLoading(true)
+      const newCommits = await getCommits()
+      if (newCommits) {
+        setCurrentCommits(newCommits)
+      }
+    } catch (e) {
+      setError(e)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+      <main className="relative flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           <span className="text-blue-600">
             There are
           </span>
-          {` ${props.commits.length} commits`}
+          {` ${currentCommits.length} commits`}
         </h1>
-
         <div className='mt-6'>
           <button
             className='bg-gray-200 border hover:border-black p-3 rounded-xl hover:bg-gray-200 active:bg-gray-300 '
-            onClick={() => { console.log('click') }}
+            disabled={loading}
+            onClick={loadNewCommitsList}
           >
-            <UpdateIcon />
+            { loading
+              ? <Loader />
+              : <UpdateIcon />
+              }
           </button>
+        
         </div>
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          {
-            props.commits.map(item => {
-              return (
-                <div
-                  key={item.sha}
-                  className='mt-6 container rounded-xl
+        {
+          error
+            ? <>{JSON.stringify(error)}</>
+            : <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+              {currentCommits.map(item => {
+                return (
+                  <div
+                    key={item.sha}
+                    className='mt-6 container rounded-xl
                   border p-6 text-left 
                   hover:border-blue-600 
                   hover:text-blue-600 
                   focus:text-blue-600 
-                  focus:border-blue-600 
+                  focus:border-blue-600
                   cursor-pointer'
-                >
-                  <h3 className="text-2xl">
-                    {item.commit.message}
-                  </h3>
-                  <div className='flex justify-between'>
-                    <p className="mt-4 text-base">
-                      {`by ${item.commit.author.name}`}
-                    </p>
-                    <p className="mt-4 text-xl text-slate-500">
-                      {formatDate(item.commit.author.date)}
-                    </p>
+                  >
+                    <h3 className="text-2xl">
+                      {item.commit.message}
+                    </h3>
+                    <div className='flex justify-between'>
+                      <p className="mt-4 text-base">
+                        {`by ${item.commit.author.name}`}
+                      </p>
+                      <p className="mt-4 text-xl text-slate-500">
+                        {formatDate(item.commit.author.date)}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              )
-            })
-          }
-        </div>
-
+                )
+              })
+              }
+            </div>
+        }
       </main>
     </div>
   )
--- src/fetch/commits.ts
+++ src/fetch/commits.ts
@@ -43,10 +43,10 @@ const octokit = new Octokit({
 })
 
 export const getCommits = async () => {
-  const test = await octokit.request('GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,per_page,page}', {
-    owner: 'sataller',
-    repo: 'git-commit-history'
+  const response = await octokit.request('GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,per_page,page}', {
+    owner: process.env.OWNER,
+    repo: process.env.REPO
   })
-  const data = await test.data
+  const data = await response.data
   return data
 }
